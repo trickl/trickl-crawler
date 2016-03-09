30bf9e42ab878328c568b08ee704018c694a3847
Tim Gee <writetotimgee@gmail.com>
2016-03-09 11:21:32 +0000

    Make parser factory configurable.

--- src/main/java/com/trickl/crawler/robot/StandardDroid.java
+++ src/main/java/com/trickl/crawler/robot/StandardDroid.java
@@ -20,10 +20,8 @@ import com.trickl.crawler.api.Worker;
 import com.trickl.crawler.handle.TaskResultHandler;
 import com.trickl.crawler.parser.ParserFactory;
 import com.trickl.crawler.parser.html.DocumentBuilder;
-import com.trickl.crawler.parser.html.HtmlCleanerDocumentBuilder;
 import com.trickl.crawler.parser.html.HtmlParser;
 import com.trickl.crawler.parser.html.JTidyDocumentBuilder;
-import com.trickl.crawler.parser.html.NekoHtmlDocumentBuilder;
 import com.trickl.crawler.parser.json.JsonParser;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,7 +31,7 @@ import java.util.List;
 public class StandardDroid<T extends Task> implements Droid<T>
 {
    protected List<TaskResultHandler> outputHandlers = new
-           ArrayList<TaskResultHandler>();
+           ArrayList<>();
    
    protected ParserFactory parserFactory;
    
@@ -41,16 +39,18 @@ public class StandardDroid<T extends Task> implements Droid<T>
 
    public StandardDroid()
    {
+      // The default parser factory, can be overriden using config
       parserFactory = new ParserFactory();
-      Parser htmlParser = new HtmlParser(Arrays.asList(new DocumentBuilder [] {
-          // Why settle for one when we can wash HTML through all three parsers
-          //new NekoHtmlDocumentBuilder(),
-          //new HtmlCleanerDocumentBuilder(), 
+      Parser htmlParser = new HtmlParser(Arrays.asList(new DocumentBuilder [] {       
           new JTidyDocumentBuilder()})
       );
       parserFactory.setMap(new HashMap<String, Object>());
       parserFactory.getMap().put("text/html", htmlParser);
       parserFactory.getMap().put("application/json", new JsonParser());      
+   }
+   
+   public void setParserFactory(ParserFactory parserFactory) {
+      this.parserFactory = parserFactory;
    }
    
    public boolean getForceAllow() {
