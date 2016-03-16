e913c97677029b5a17945adb5c5d151d7f5673df
Tim Gee <writetotimgee@gmail.com>
2016-03-16 13:49:26 +0000

    Increase mask size of processed HTML from 0.5Mb to 2Mb

--- src/main/java/com/trickl/crawler/protocol/http/TaskHttpClient.java
+++ src/main/java/com/trickl/crawler/protocol/http/TaskHttpClient.java
@@ -1,7 +1,9 @@
 package com.trickl.crawler.protocol.http;
 
 import org.apache.droids.protocol.http.DroidsHttpClient;
+import static org.apache.droids.protocol.http.DroidsHttpClient.MAX_BODY_LENGTH;
 import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.params.HttpParams;
 
 public class TaskHttpClient extends DroidsHttpClient
 {
@@ -9,5 +11,13 @@ public class TaskHttpClient extends DroidsHttpClient
   protected HttpRequestRetryHandler createHttpRequestRetryHandler()
   {
     return new TaskHttpRequestRetryHandler(3);
+  }
+  
+  @Override
+  protected HttpParams createHttpParams()
+  {
+    HttpParams params = super.createHttpParams();
+    params.setLongParameter(MAX_BODY_LENGTH, 2 * 1024 * 1024); // 2Mb
+    return params;
   }
 }
